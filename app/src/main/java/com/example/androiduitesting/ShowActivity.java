81c0d7fde226974e98b9cd04b24f76b621308e6f
Kristian Perez <kris.ilijay.perez@gmail.com>
2025-10-23 23:41:31 -0600

    feat: testing works

---
+++ app/src/main/java/com/example/androiduitesting/ShowActivity.java
@@ -0,0 +1,38 @@
+package com.example.androiduitesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ShowActivity extends AppCompatActivity {
+
+    TextView cityTextView;
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show);
+
+        cityTextView = findViewById(R.id.textView_cityName);
+        backButton = findViewById(R.id.button_back);
+
+        Intent intent = this.getIntent();
+        String cityName = intent.getStringExtra("CITY_NAME");
+
+        if (cityName != null) {
+            cityTextView.setText(cityName);
+        }
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
